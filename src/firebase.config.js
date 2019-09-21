6408df3fe32be313eb8e05b362afa3dde6ed2525
Paul Krisadaolrn <pauly.k@gmail.com>
2019-09-21 21:16:55 +0700

    remove firebase.config.js from .gitignore

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+export default {
+	apiKey: "AIzaSyBjJvhJeRalRHkWOL2CWRLWQdUr3QJ_Ieo",
+	authDomain: "scratchpad-v1.firebaseapp.com",
+	databaseURL: "https://scratchpad-v1.firebaseio.com",
+	projectId: "scratchpad-v1",
+	storageBucket: "scratchpad-v1.appspot.com",
+	messagingSenderId: "327597118117",
+	appId: "1:327597118117:web:9fb6e3ca2776df6836b34c"
+};
+
